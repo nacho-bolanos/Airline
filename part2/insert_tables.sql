a168c730e000d1e7b5655e3a48db4ae71a18189c
Jeaha31 <94806973+Jeaha31@users.noreply.github.com>
2021-11-29 16:03:39 -0500

    Update insert_tables.sql
    changed "password" to md5("password")

--- part2/insert_tables.sql
+++ part2/insert_tables.sql
@@ -7,13 +7,13 @@ insert into airport (code, name, city) values ("JFK", "John F. Kennedy Internati
 insert into airport (code, name, city) values ("PVG", "Shanghai Pudong International Airport", "Shanghai");
 
 insert into customer (email, password, name, adrr_building_num, adrr_street, addr_city, addr_state, phone_num, passport_num, passport_exp, passport_country, date_of_birth) 
-values ("jose@gmail.com", "himynameisjose", "Jose Bolanos", 6, "Metrotech", "Brooklyn", "New York", 1234567, 111111, '2024-12-20', "Guatemala", '2000-09-21');
+values ("jose@gmail.com", md5("himynameisjose"), "Jose Bolanos", 6, "Metrotech", "Brooklyn", "New York", 1234567, 111111, '2024-12-20', "Guatemala", '2000-09-21');
 
 insert into customer (email, password, name, adrr_building_num, adrr_street, addr_city, addr_state, phone_num, passport_num, passport_exp, passport_country, date_of_birth) 
-values ("jaeha@gmail.com", "himynameisjaeha", "Jaeha Huh", 6, "Metrotech", "Brooklyn", "New York", 128723947, 112344, '2024-04-12', "United States", '2000-04-15');    
+values ("jaeha@gmail.com", md5("himynameisjaeha"), "Jaeha Huh", 6, "Metrotech", "Brooklyn", "New York", 128723947, 112344, '2024-04-12', "United States", '2000-04-15');    
 
 insert into customer (email, password, name, adrr_building_num, adrr_street, addr_city, addr_state, phone_num, passport_num, passport_exp, passport_country, date_of_birth) 
-values ("mat@gmail.com", "himynameismat", "Mat Brown", 6, "Metrotech", "Brooklyn", "New York", 29759472, 1287398, '2023-11-16', "United States", '1999-12-30');
+values ("mat@gmail.com", md5("himynameismat"), "Mat Brown", 6, "Metrotech", "Brooklyn", "New York", 29759472, 1287398, '2023-11-16', "United States", '1999-12-30');
 
 insert into airplane (id, seats) values ("Boeing 737-800", 160);
 insert into airplane (id, seats) values ("Boeing 737-700", 143);
@@ -24,7 +24,7 @@ insert into own (airline_name, id) values ('China Eastern', 'Boeing 737-700');
 insert into own (airline_name, id) values ('China Eastern', 'Airbus A320ceo');
 
 insert into airline_staff (username, password, fname, lname) 
-values ("john@gmail.com", "youcantseeme", "John", "Cena");
+values ("john@gmail.com", md5("youcantseeme"), "John", "Cena");
 
 insert into works (username, airline_name) values ("john@gmail.com", "China Eastern");
 
